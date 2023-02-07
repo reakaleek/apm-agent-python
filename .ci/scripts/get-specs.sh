dc267bb3d00cd31a915f36cdf0621752be8367a7
Jan Calanog <jan.calanog@elastic.co>
2023-02-07 15:59:10 +0100

    ok

--- .ci/scripts/get-specs.sh
+++ .ci/scripts/get-specs.sh
@@ -5,8 +5,13 @@ set -euo pipefail
 readonly REPO_DIR=".ci/apm-data"
 rm -rf "${REPO_DIR}"
 mkdir -p "${REPO_DIR}"
+
+(
 cd "${REPO_DIR}"
 git init
 git remote add origin https://github.com/elastic/apm-data.git
-git fetch --depth 1 origin "${1}"
+git fetch --depth 1 origin "${2}"
 git checkout FETCH_HEAD
+)
+
+cp "${REPO_DIR}/input/elasticapm/docs/spec" "${1}"
